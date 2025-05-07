9d87ce45845d5463b4206623adf25c04da0ba954
ivan2x <ivancm997@gmail.com>
2025-05-07 17:55:17 -0600

    dependencies added

---
+++ src/main/java/org/ivn/App.java
@@ -0,0 +1,13 @@
+package org.ivn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
